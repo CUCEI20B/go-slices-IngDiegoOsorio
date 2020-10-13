436348ad3bc4109faa9884a996aefe58b327a4f2
Diego Osorio <juan.osorio1599@alumnos.udg.mx>
2020-10-12 23:15:49 -0500

    Prueba 1

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
-}
+	var n int
+	var s [] uint16
+	var total uint16
+	fmt.Scanln(&n)
+
+	for i := 0 ; i < n; i++ {
+		var entrada uint16
+		fmt.Scanln(&entrada)
+		s = append(s, entrada)
+	}
+	
+	for _, v := range s {
+		total += v
+	}
+
+	fmt.Println(total)
+ }
